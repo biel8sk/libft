3c1d78d07bf3b0e1e99693b13933eac1ead82ba1
Gabriel Pires Cardim Barreto <gpires-c@c1r3p1.42sp.org.br>
2026-05-30 22:12:37 -0300

    updates in ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,50 +6,34 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 20:06:23 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/05/30 20:54:55 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/05/30 21:15:18 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_occurrence(char *str, char *set)
-{
-	size_t	i;
-	size_t	len_little;
-	int		count;
-
-	i = 0;
-	len_little = ft_strlen(set);
-	if (!*set)
-		return (0);
-	while (str[i])
-	{
-		if (ft_strncmp(str, set, len_little) == 0)
-			return (count);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len_s;
+	size_t	len_trimmed;
 	char	*str_trimmed;
 	int		i;
+	size_t	len_set;
 
-	len_s = 0;
+	len_trimmed = 0;
 	i = 0;
+	len_set = ft_strlen(set);
 	while (*s1)
 	{
-		while (*s1 != *set)
-			len_s++;
-		s1++;
+		if (*s1 == *set && ft_strncmp(s1, set, len_set))
+			s1++;
+		len_trimmed++;
 	}
-	str_trimmed = (char *)malloc(len_s +1);
+	str_trimmed = (char *)malloc(len_trimmed +1);
 	while (s1[i])
 	{
 		while (s1[i] != *set)
-			str_trimmed[i] = *s1;
+			str_trimmed[i] = s1[i];
 		i++;
 	}
+	return (str_trimmed);
 }
