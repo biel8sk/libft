9934c8a049084e22ef7f270f2fcc59e8cfd10d22
Gabriel Pires Cardim Barreto <gpires-c@c3r4p3.42sp.org.br>
2026-06-02 19:22:53 -0300

    commit

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 21:25:00 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/01 21:20:40 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/02 19:12:49 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 		return (src_len);
 	while (dest[j])
 		j++;
-	while (src[i] && i < n -1)
+	while (src[i] && i < n)
 	{
 		dest[j + i] = src[i];
 		i++;
