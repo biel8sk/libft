56d2cbb84c954ecbce56006c70d71f72b749784b
Gabriel Pires Cardim Barreto <gpires-c@c3r4p3.42sp.org.br>
2026-06-02 19:55:05 -0300

    fix norminete and ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,40 +6,66 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 22:27:11 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/01 19:21:15 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/02 19:52:25 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_power_ten(int n, int *count)
-{
-	int	mount;
+#include "libft.h"
 
-	count = 0;
-	mount = 10;
-	if (n < 10)
-		return (0);
-	while (n > mount)
+int	get_len(int n)
+{
+	int	len;
+
+	len = 1;
+	while (n > 10)
 	{
-		mount *= 10;
-		count ++;
+		n /= 10;
+		len ++;
 	}
-	return (mount);
+	return (len);
+}
+
+void	calc_operations_and_set_char(char *a, int n, int len)
+{
+	int	temp;
+	int	power;
+	int	i;
+
+	i = 0;
+	while (len > 0)
+	{
+		temp = n;
+		power = 1;
+		while (temp > 10)
+		{
+			temp /= 10;
+			power *= 10;
+		}
+		n -= temp * power;
+		temp += 48;
+		a[i] = temp;
+		len--;
+		i++;
+	}
+	a[i] = '\0';
 }
 
 char	*ft_itoa(int n)
 {
-	// decobrir quantas casas tem o numero
-	// allocar na memoria um a string
-	// pra cada casa 
-	//	calcular a potencia necessária para que a subtracao do numero
-	//	seja maior ou igual a 0
-	//	subtrair da maior potencia e subtrair o resto da potencia passan
-	//	o resto da subtracao anterior
-	//	contar quantas casas precisa ate chegar na potencia
-	//
-	// pela soma da potencia e verificar se é 
-	// menor 
-	// 
-	// pra pegar o ultimo valor
-	// 
+	char	*a;
+	int		len;
+
+	len = get_len(n);
+	a = (char *)malloc(len * sizeof(char) + 1);
+	calc_operations_and_set_char(a, n, len);
+	return (a);
 }
+
+
+// int	main(void)
+// {
+// 	#include "stdio.h"
+
+// 	printf("%s ", ft_itoa(123));
+// 	printf("%s ", ft_itoa(48928));
+// }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 19:52:48 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/01 21:53:38 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/02 19:45:59 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,16 @@ int	count_delimiter(char const *s, char c)
 	return (count);
 }
 
+static int	calc_len_to_increment(const char *main_str, char *curr_str, int i)
+{
+	int	len;
+
+	len = ft_strlen((const char *) curr_str);
+	if (main_str[len + i])
+		return (len + 1);
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**str_splited;
@@ -52,21 +62,15 @@ char	**ft_split(char const *s, char c)
 	int		count_c;
 	int		i;
 	int		j;
-	int		len;
 
 	i = 0;
 	j = 0;
-	len = 0;
 	count_c = count_delimiter(s, c);
 	str_splited = (char **)malloc((sizeof(char *) * (count_c + 2)));
 	while (s[j])
 	{
 		str = alloc_s(&s[j], c);
-		len = ft_strlen(str);
-		if (s[j + len])
-			j += len + 1;
-		else
-			j += len;
+		j += calc_len_to_increment((const char *)&s[j], str, j);
 		str_splited[i] = str;
 		i++;
 	}
@@ -74,15 +78,14 @@ char	**ft_split(char const *s, char c)
 	return (str_splited);
 }
 
-int	main(void)
-{
+// int	main(void)
+// {
+// 	char *str = "skate,or,die";
 
-	char *str = "skate,or,die";
-
-	char **arr_str = ft_split(str, ',');
-	#include "stdio.h"
-	for (int i =0; i < 3; i++)
-	{
-		printf("%s ", arr_str[i]);
-	}
-}
+// 	char **arr_str = ft_split(str, ',');
+// 	#include "stdio.h"
+// 	for (int i =0; i < 3; i++)
+// 	{
+// 		printf("%s ", arr_str[i]);
+// 	}
+// }
