64d35341cbb0a878dfa28e18d6f111687a0ee67c
Gabriel Pires Cardim Barreto <gpires-c@c1r6p3.42sp.org.br>
2026-06-04 22:55:36 -0300

    fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 21:25:00 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/02 19:12:49 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/04 22:53:16 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,28 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
-	size_t	j;
 	size_t	src_len;
 
 	i = 0;
-	j = 0;
 	src_len = ft_strlen(src);
 	if (n == 0)
 		return (src_len);
-	while (dest[j])
-		j++;
-	while (src[i] && i < n)
+	if (n > src_len + 1)
 	{
-		dest[j + i] = src[i];
-		i++;
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
 	}
-	dest[i + j] = '\0';
+	else
+	{
+		while (i < n -1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	dest[i] = '\0';
 	return (src_len);
 }
