84cba5189d19885a21c8b8874176ea8d43a845ef
Gabriel Pires Cardim Barreto <gpires-c@c1r6p3.42sp.org.br>
2026-06-04 23:23:06 -0300

    fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 21:42:44 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/03 20:42:07 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/04 23:20:54 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,30 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dst_size)
 {
 	size_t	i;
 	size_t	len_dest;
+	size_t	len_src;
 
 	i = 0;
+	len_src = ft_strlen(src);
 	len_dest = ft_strlen(dest);
-	if (len_dest < dst_size)
+	if (dst_size == 0)
+		return (len_src);
+	if (len_dest >= dst_size)
+		return (dst_size + len_src);
+	while (src[i] && i < dst_size - len_dest -1)
 	{
-		while (src[i] && i < dst_size - len_dest -1)
-		{
-			dest[len_dest + i] = src[i];
-			i++;
-		}
-		if (!dest[len_dest + i])
-			dest[len_dest + i] = '\0';
+		dest[i + len_dest] = src[i];
+		i++;
 	}
+	dest[i + len_dest] = '\0';
 	return (len_dest + ft_strlen(src));
 }
+	// if (len_dest < dst_size)
+	// {
+	// 	while (src[i] && i < dst_size - len_dest -1)
+	// 	{
+	// 		dest[len_dest + i] = src[i];
+	// 		i++;
+	// 	}
+	// 	if (!dest[len_dest + i])
+	// 		dest[len_dest + i] = '\0';
+	// }
