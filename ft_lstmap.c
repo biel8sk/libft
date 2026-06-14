c3049321cb6b12f2a673ab9b258258ba79fb12d3
Gabriel Pires Cardim Barreto <gpires-c@c1r5p1.42sp.org.br>
2026-06-14 13:12:44 -0300

    feat: all functions working

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 21:04:34 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/06/12 21:40:27 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/06/14 13:11:16 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
+	t_list	*ret;
 
 	new = NULL;
+	ret = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
 	while (lst->next != NULL)
@@ -24,11 +26,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new = ft_lstnew(f(lst->content));
 		if (!new)
 			ft_lstdelone(new, del);
+		ft_lstadd_back(&ret, new);
 		lst = lst->next;
-		new = new->next;
 	}
 	new = ft_lstnew(f(lst->content));
 	if (!new)
 		ft_lstdelone(new, del);
-	return (new);
+	ft_lstadd_back(&ret, new);
+	return (ret);
 }
